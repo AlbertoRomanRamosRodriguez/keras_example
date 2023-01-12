34568bcfe1bb995e7621f18a1affaa751bba00da
Alberto Román Ramos Rodríguez <rr.albertoroman@gmail.com>
2023-01-12 09:22:45 -0500

    fixed bugs in ImageToArrayPreprocessor

--- preprocessing/imagetoarrayprocessor.py
+++ preprocessing/imagetoarrayprocessor.py
@@ -1,8 +1,8 @@
 from tensorflow.keras.preprocessing.image import img_to_array
 
 class ImageToArrayPreprocessor:
-    def __init__(self, dataFormat) -> None:
+    def __init__(self, dataFormat = None) -> None:
         self.dataFormat = dataFormat
     
     def preprocess(self, image):
-        return image_to_array(image, data_format=self.dataFormat)
+        return img_to_array(image, data_format=self.dataFormat)
