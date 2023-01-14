9d92d1d735ed730ed1eabf8845a3d596ff01e39f
Alberto Román Ramos Rodríguez <rr.albertoroman@gmail.com>
2023-01-14 14:10:42 -0500

    created a new head for the model

---
+++ nn/conv/fcheadnet.py
@@ -0,0 +1,13 @@
+from keras.layers import Dropout, Flatten, Dense
+
+class FCHeadNet:
+    @staticmethod
+    def build(baseModel, classes, D):
+        headModel = baseModel.output
+        headModel = Flatten(name="flatten")(headModel)
+        headModel = Dense(D, activation="relu")(headModel)
+        headModel = Dropout(0.5)(headModel)
+
+        headModel = Dense(classes, activation="softmax")(headModel)
+
+        return headModel
